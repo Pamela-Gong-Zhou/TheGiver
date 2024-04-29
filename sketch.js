b96423564213de8ffe351f84589bcf06623e4f03
Pamela-Gong-Zhou <145486723+Pamela-Gong-Zhou@users.noreply.github.com>
2024-04-29 00:24:07 -0700

    updated sketch

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var xspeed = 5;
 var xpos = 75;
 
 function setup() {
-  createCanvas(1400, windowHeight);
+  createCanvas(1345, windowHeight);
 }
 
 function draw() {
@@ -141,5 +141,28 @@ function draw() {
   fill(0);
   ellipse(xpos + 1360, 180, 45, 45);
   }
+
+  // let string = 'Our people made that choice, the choice to go to Sameness. Before my time, before the previous time, back and back and back. We relinquished color when we relinquished sunshine and did away with a difference. We gained control of many things. But we had to let go of others.';
+
+  // let currentCharacter = 0;
+  // let pageMargin = 25;
+
+  // let currentString = string.substring(0, currentCharacter);
+  // push();
+  // fill(0);
+  // noStroke();
+  // rect(pageMargin, pageMargin, width - pageMargin*2, height - pageMargin);
+  // pop();
+  
+  // push();
+  // textSize(12);
+  // fill("white");
+  // textFont(`Courier`);
+  // textLeading(30);
+  // textAlign(LEFT, TOP);
+  // text(currentString, pageMargin + 10, pageMargin + 10, width - pageMargin*2, height - pageMargin);
+  // pop();
+  
+  // currentCharacter += 0.5;  
 }
 
---
+++ sketchone.js
@@ -0,0 +1,32 @@
+// let string = 'Our people made that choice, the choice to go to Sameness. Before my time, before the previous time, back and back and back. We relinquished color when we relinquished sunshine and did away with a difference. We gained control of many things. But we had to let go of others.';
+
+// let currentCharacter = 0;
+
+// let pageMargin = 300;
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+// }
+
+// function draw() {
+//   background(0);
+  
+//   let currentString = string.substring(0, currentCharacter);
+  
+//   push();
+//   fill(0);
+//   noStroke();
+//   rect(pageMargin, pageMargin, width - pageMargin*2, height - pageMargin);
+//   pop();
+  
+//   push();
+//   textSize(36);
+//   fill("white");
+//   textFont(`Courier`);
+//   textLeading(30);
+//   textAlign(LEFT, TOP);
+//   text(currentString, pageMargin + 10, pageMargin + 10, width - pageMargin*2, height - pageMargin);
+//   pop();
+  
+//   currentCharacter += 0.5;
+// }
