fab05743d7f3f4a4da0d5f025952247a574823b4
Pamela-Gong-Zhou <145486723+Pamela-Gong-Zhou@users.noreply.github.com>
2024-04-30 11:21:04 -0700

    updates

--- sketch.js
+++ sketch.js
@@ -8,6 +8,9 @@ function setup() {
 function draw() {
   
   background(0);
+
+  letters(); 
+
   frameRate(12);
   stroke("white");
   
@@ -142,27 +145,32 @@ function draw() {
   ellipse(xpos + 1360, 180, 45, 45);
   }
 
-  // let string = 'Our people made that choice, the choice to go to Sameness. Before my time, before the previous time, back and back and back. We relinquished color when we relinquished sunshine and did away with a difference. We gained control of many things. But we had to let go of others.';
 
-  // let currentCharacter = 0;
-  // let pageMargin = 25;
+}
 
-  // let currentString = string.substring(0, currentCharacter);
-  // push();
-  // fill(0);
-  // noStroke();
-  // rect(pageMargin, pageMargin, width - pageMargin*2, height - pageMargin);
-  // pop();
+function letters() {
+
+  let string = 'Our people made that choice, the choice to go to Sameness. Before my time, before the previous time, back and back and back. We relinquished color when we relinquished sunshine and did away with a difference. We gained control of many things. But we had to let go of others.';
+
+let currentCharacter = 0;
+let pageMargin = 25;
+
+  let currentString = string.substring(0, currentCharacter);
+  push();
+  fill(0);
+  noStroke();
+  rect(pageMargin, pageMargin, width - pageMargin*2, height - pageMargin);
+  pop();
   
-  // push();
-  // textSize(12);
-  // fill("white");
-  // textFont(`Courier`);
-  // textLeading(30);
-  // textAlign(LEFT, TOP);
-  // text(currentString, pageMargin + 10, pageMargin + 10, width - pageMargin*2, height - pageMargin);
-  // pop();
+  push();
+  textSize(12);
+  fill('white');
+  textFont(`Courier`);
+  textLeading(30);
+  textAlign(LEFT, TOP);
+  text(currentString, pageMargin + 10, pageMargin + 10, width - pageMargin*2, height - pageMargin);
+  pop();
   
-  // currentCharacter += 0.5;  
+  currentCharacter += 1;  
 }
 
--- type.js
+++ type.js
@@ -1,32 +1,38 @@
-// let string = 'Our people made that choice, the choice to go to Sameness. Before my time, before the previous time, back and back and back. We relinquished color when we relinquished sunshine and did away with a difference. We gained control of many things. But we had to let go of others.';
+let string = 'Although we had through the memories learned about the pain of loss and loneliness, now we gained, too, an understanding of solitude and its joy.';
 
-// let currentCharacter = 0;
+let currentCharacter = 0;
 
-// let pageMargin = 300;
+let pageMargin = 300;
 
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-// }
+let font;
 
-// function draw() {
-//   background(0);
+function preload (){
+    font = loadFont ('Netto Brushes.otf');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
   
-//   let currentString = string.substring(0, currentCharacter);
+  let currentString = string.substring(0, currentCharacter);
   
-//   push();
-//   fill(0);
-//   noStroke();
-//   rect(pageMargin, pageMargin, width - pageMargin*2, height - pageMargin);
-//   pop();
+  push();
+  fill(0);
+  noStroke();
+  rect(pageMargin, pageMargin, width - pageMargin*2, height - pageMargin);
+  pop();
   
-//   push();
-//   textSize(36);
-//   fill("white");
-//   textFont(`Courier`);
-//   textLeading(30);
-//   textAlign(LEFT, TOP);
-//   text(currentString, pageMargin + 10, pageMargin + 10, width - pageMargin*2, height - pageMargin);
-//   pop();
+  push();
+  textSize(100);
+  fill("white");
+  textFont(`font`);
+  textLeading(90);
+  textAlign(LEFT, TOP);
+  text(currentString, pageMargin + 10, pageMargin + 10, width - pageMargin*2, height - pageMargin);
+  pop();
   
-//   currentCharacter += 0.5;
-// }
+  currentCharacter += 0.3;
+}
