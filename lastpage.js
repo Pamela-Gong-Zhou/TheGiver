5b613079d18b996cc1846850460bc68df1303d52
Pamela-Gong-Zhou <145486723+Pamela-Gong-Zhou@users.noreply.github.com>
2024-05-02 10:24:49 -0700

    updates on last page

--- lastpage.js
+++ lastpage.js
@@ -1,14 +1,32 @@
 function setup() {
     createCanvas (windowWidth, windowHeight);
-    background (0);
+    background ("#50C878");
   }
 
 function draw(){
-    fill("white");
-    text('The End?', mouseX, mouseY);
-    textSize(50);
+    fill(0);
+    textSize(100);
+    textFont('Netto Brushes');
+    text('This is', width/3.8, height/1.8);
+
+
+    drawEnd();
+    drawStart();
 
     if (mouseIsPressed){
-        background(0);
+        background("#50C878");
     }
+}
+
+function drawEnd(){
+    fill("white");
+    textFont('anaphora');
+    text('The End?', mouseX, mouseY);
+    textSize(100);
+}
+
+function drawStart(){
+    fill("#50C878");
+    textFont('Netto Brushes');
+    text('The Beginning...', width/2.4, height/1.8);
 }
