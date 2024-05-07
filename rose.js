7b927c2914ae2a21a8b74d1ce63d28920af47c39
Pamela-Gong-Zhou <145486723+Pamela-Gong-Zhou@users.noreply.github.com>
2024-05-07 09:39:15 -0700

    updates

--- rose.js
+++ rose.js
@@ -1,12 +1,16 @@
 let draft, ready;
+var img;
 
 function preload(){
   ready = loadImage ('rose.jpg');
   draft = loadImage ('bwrose.jpg');
+  img = loadImage('Brush.png');
 }
 
 function setup() {
   createCanvas(600, windowHeight);
+  noCursor(); 
+  cursor('Brush.png', 25, 25); 
   image (ready, 0, 0);
   image (draft, 0, 0);
 }
